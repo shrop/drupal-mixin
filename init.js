9a087ac1f02c681d22e2c54ada66a6665193ac4c
Mark Shropshire <mdshrops@shropnet.net>
2013-12-16 07:02:58 -0500

    Improved the Drush clear all drupal caches functionality

--- init.js
+++ init.js
@@ -6,38 +6,37 @@ Hooks.addMenuItem("Actions/Drupal/Clear Drupal caches", "opt-cmd-x", function() 
   
   exec('cd ' + dirPath + ' && drush cc all', function (err, stdout, stderr) {
     if (err)  {
-      Alert.show('Error: ' + err);
+      Alert.show(err);
       return false;
     }
-    else if (stderr.indexOf("'all' cache was cleared.") == -1) {
+    else if (stderr.indexOf("'all' cache was cleared") == -1) {
       Alert.show('Drush failed to clear all caches.');
       return false;
     }
     else {
+      console.log(dirPath);
       Alert.notify({
         title: "Drupal caches cleared!",
-        body: dirpath,
+        body: "" + dirPath + ""
       });
     };
   });
 });
 
-// Coder Review.
+// Code review the current .module file.
 Hooks.addMenuItem("Actions/Drupal/Coder Review", "opt-cmd-r", function () {
   var exec = require('child_process').exec;
   var path = require('path');
-  var dirPath = path.dirname(Document.current().path());
+  var documentPath = Document.current().path();
+  var dirPath = path.dirname(documentPath);
   
-  exec('cd ' + dirPath + ' && drush coder --no-empty --minor --comment --i18n --security --sql --style --druplart --sniffer --ignorename', function (err, stdout, stderr) {
+  exec('cd ' + dirPath + ' && drush coder --no-empty --minor --comment --i18n --security --sql --style --druplart --sniffer --ignorename ' + documentPath, function (err, stdout, stderr) {
     if (err)  {
-      Alert.show('Error: ' + err);
+      Alert.show(err);
       return false;
     }
     else {
-      Alert.notify({
-        title: "Drupal caches cleared!",
-      });
+      Alert.show(stdout);
     };
   });
-
 });
