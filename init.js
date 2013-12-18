48b08965e8e0172d3a0993e013438ba2e230893e
Mark Shropshire <mdshrops@shropnet.net>
2013-12-18 14:32:29 -0500

    Added Drupal version and got Coder review to work
    * Still need to pretty up coder review results though

--- init.js
+++ init.js
@@ -10,7 +10,7 @@ Hooks.addMenuItem("Actions/Drupal/Clear Drupal caches", "opt-cmd-x", function() 
       return false;
     }
     else if (stderr.indexOf("'all' cache was cleared") == -1) {
-      Alert.show('Drush failed to clear all caches.');
+      Alert.show("Drush failed to clear all caches.");
       return false;
     }
     else {
@@ -18,24 +18,37 @@ Hooks.addMenuItem("Actions/Drupal/Clear Drupal caches", "opt-cmd-x", function() 
         title: "Drupal caches cleared!",
         body: "" + dirPath + ""
       });
-    };
+    }
   });
 });
 
-// Code review the current .module file.
-Hooks.addMenuItem("Actions/Drupal/Coder Review", "opt-cmd-r", function () {
+// Code review the current file.
+Hooks.addMenuItem("Actions/Drupal/Coder review", "opt-cmd-r", function () {
   var exec = require('child_process').exec;
   var path = require('path');
   var documentPath = Document.current().path();
-  var dirPath = path.dirname(documentPath);
-  
+  var dirPath = path.dirname(Document.current().path());
+
   exec('cd ' + dirPath + ' && drush coder --no-empty --minor --comment --i18n --security --sql --style --druplart --sniffer --ignorename ' + documentPath, function (err, stdout, stderr) {
+    var win = new Window();
+    win.title = "Coder review results";
+    win.html = stdout;
+    win.run();
+    win.maximize();
+  });
+});
+
+// Return the current drush version.
+Hooks.addMenuItem("Actions/Drupal/Drush version", "", function () {
+  var exec = require('child_process').exec;
+
+  exec('drush --version', function (err, stdout, stderr) {
     if (err)  {
       Alert.show(err);
       return false;
     }
     else {
       Alert.show(stdout);
-    };
+    }
   });
 });
